a90d62547c8f5b6b4031ad28a325147d357148aa
Vlad Patryshev <vpatryshev@gmail.com>
2020-05-25 22:10:15 -0700

    Added measure

--- src/js/fingertree.js
+++ src/js/fingertree.js
@@ -1,4 +1,3 @@
-// link: https://sourceacademy.nus.edu.sg/playground#chap=4&exec=1000&ext=NONE&prgrm=MYewdgzgLgBA1gUwJ4wLwwBYIIYBMDcAUKJLAG7YA2ArgmjFNgJaVGEBm1YwUT4MJRk0gB9SgAoIAGhgAPAJQwA3oRgwATgijV1YGEwgiw1ShIiKA-DHZUIdAFwwAhAaMmzigGSeY42WlR0LDxJeUUAH3CBcCFRCSEPGQUiAF9CDi4ePj1hO3UoMT8ZOyhFFTVNbV1osFjDMy0kyxgSmEcAB2Z1IpateVT0zm5efi5swpKARmKtACYy1Q0tHRzDY1NJLUnmktm2mDHwQoTNqG2ZXIR8wuDcU-PeqHn%2BwjSM4ez9Wqu7LKOGs4zJ4LNRMdi%2BVzrAHbGCRfRrdynZ7KJZVPRQ-AwNJqBCUOzKRZqEjQOT0W73F5qIngEmaEnoYRQH4IP5gY7MaHyIHzIhU1ErGp1CZzJowKydJjdWQyOmlfay3lY17pYmwSggEBwajtei4Jg8NAAPngRpgixcCI2ep4Xh84kQSHE5OtpUUgXQcDFZr5FBoCCdODuLrC%2B3Vmu14hOwfk9rCirUhLNqq%2BeQKLvo9pkZDdxvTqGNEu6hczMGzMmDbGTACNqCxcCI8zBKAZYPn4W4Ns3oM0oW1E3zQZArmn9VB7cgAyEu66pL7aJO7tOwjGa3WG6PIxzxEu4yqabBtbhsEz1wb0HnjSXs6byjAAPR3mAASXBYBAsHYIC4uC%2BMBd2WwdQUAAdzoCAMC-SgfzwH8mFgRYwXESFEWjW8%2BQfGAwAQBAfygEB9AAW3aSgEAIhBan7flqkmRVsRgXF8UQi0OwkYMYG8XwHQXcRowCD0wjQqkShEE5nVHLlS0pJUcTxBBBLUQ9jwQU8xyjcSYzgWcpLoyoBRdVJK33TAuAAc0AphsDZYCQHUXAIHoVcoJU7cW3EYtphgAAiBATKQTyJOLWYZE8xAoCgABX-yZGLABmYKMAAQ-UEACP83diCMjBTPMyz6DDLV2idbL1AsqybLsl53lZTBsHA8Q5wQMoqL0BqYAAUhgGKaKVKqRj0QRmFEDAICdWrhsaOQQX0cEkMtCQMDGkoBMTXTqhsWTaMWBi5MTZMHTJMa-Ck6lSAEBbhDyjUCtG8ClvHJBjua5xkI2YBzrAW1BUG%2BpxDewamk2t4hmqy5rmG05RRURMMIACTG58wFwBB-DfD8v0RxNEPyiM7oWuqFDdQJrFsRr5KpB8AFlsEQTCEGAx5KJOkksOAsQWzy1zizxjAjpkacjt3AcqWTFmRFadBDjZCRRenIFHr5Vb0TpsWtHjaSqW2sm1AfABBXBcPw5GW2EEyGaFpnYBAJzxabK6cb6G6eYJtW%2BRF5WbdBgoJGlGArfrJaXapRWDnaI8TxdU4Y253nRYDxM0iBzI%2BpTYcRElkRwd2IFtkE3ajKYH8gkO8kpnS9DHwAZS2Gr7O5r4kZR99f12TH2G3O3Ct2GMC8J9B1rseQtaez3hUBLu1bomS7CH5M-ZVp5LvDTu5m73B5eFozY7meh04kOes4YLdS-XiplmqRTw43LukNwKQt%2BBCf0jeB8Xs5L1dn2XfNxYCkLiHa55qBl-o8Z4bBgbJ3TpnLY3IprB1fhSd%2B29HCQJGicUuf9UxpzAOMKBTwZAly2MuAYhA7wACoMqnRHnXdAToyBZhzE2dmbZixczoZJPmLZKrVlrE5ahjD6TGngUuL0vZ7CMypFQ4uxcgE7ikOSHc4hHL1mjicHcXCjL%2BHQEojOh1%2BYADY4rTD0bMAADNMGKsxpgABYgrGMmOYyYAB2KQkwTEmKkDFAAHFIAAnDFExjjdykLvHuU6w0HI8PrBHfm7kpD6MMVIYxZiwgyHEebGA7kPL8wsdY2xljtgSTSebYszj%2BFji6s41x7ivG%2BP8YEohhBhpsBIXeAAKggEkAARUcAEgIwAAKqhyUoQPUEBiLYEdFlMAZkSqWRENZWy0gvIACEECfk0AMoZTJ7D%2BSIKM8ZjoL7KQjlMmZpV5nlXMOIIKnkAAa9yopeR1uwJk6hNlhwQDsyqLSYDtJJJ7L4eEa4YHni0vWBtab01aAAVgoczZW3M2b0hTmDEaw0ZDQpePsygEzxCi0RbLLyAAdMAT5-6wGhTAAAtMaOuTBZCOGhSS3ZYL9YMENrIY20zHgwEmHC2A%2BKxpIoXgyclGcRqCvAsKmQkxZiYr2QYA5eKEVCunEFYlpLyW8rldS2l8N6WOEmMyl4us2VAqNtAE2PKYp6P5ZCnRUrpwxXoFQiVKrHUtnVTarFiqcWOklSCp1wUSVktTJ1PRurgX6AZTAJlYAWWmohRa3g3LWh6J8XagNwqrEurFeDLNQaYDpp9WMv1yrWYEpbFY4Nmqw3psjXSmNjjjXNOxbi8kBaq0SU8iSg1saW0jN9e2oBJxO3QCsSkjVfajXxpLUq8kJxR3usDV2ydPbsExvsQOttkyR1bkXVuMdUAJ3LhrX25ts7mkYT%2BbAfp2D%2BBwylSUO1w1JgRLXNEzmXRxDTHiYk0xBTUnpPSZkjh0BxA5MnUUoWxZ3FlIg0Y54UhoMDhKWB8pTiPHeL8QE5cJ9gMvogHsLRkTnIxO-dC9D4hKPhpSShvksT%2BaAe9MBmD377FUZsf%2B%2BxdHWNsclOIUp2TMNVNqbhhp6Qd1OggB5ddMMdYVxhjACuABRVpkx7DbqHZMojNb5OKeU2p2YmnL2EedRLe9bJwavvwURudZbhpxQ1Xez4j6MDKa0CZllGIgA
 const lookup = dict => key => is_null(dict) ?  undefined :
                               head(head(dict)) === key ? tail(head(dict)) :
                               lookup(tail(dict))(key);
@@ -16,19 +15,18 @@ const flatmap = f => list => flatten(map(f)(list));
 const cons = tree => tree(cons);
 const snoc = tree => tree(snoc);
 
-const $ = v => is_string(v) ? v : (!is_function(v) || (v(".") === undefined)) ? stringify(v) : v(".");
-const scan = v => (!is_function(v) || (v(scan) === undefined)) ? flatten(v) : v(scan);
-const foldl = (op, z) => v => (!is_function(v) || (v(foldl) === undefined)) ? op(z,v) : v(foldl)(op,z);
+const $ = x => is_string(x) ? x : (!is_function(x) || (x(".") === undefined)) ? stringify(x) : x(".");
+const scan = x => (!is_function(x) || (x(scan) === undefined)) ? flatten(x) : x(scan);
+const foldl = (op, z) => x => (!is_function(x) || (x(foldl) === undefined)) ? op(z,x) : x(foldl)(op,z);
+const size = v => (!is_function(v) || (v(size) === undefined)) ? 1 : v(size);
 
-function tded(a,b) {
-    return tree(digit1(b), empty, digit1(a));
-}
 const single = a => dispatch(list(
     ".",  "single(" + $(a) + ")",
     cons, b => tree(digit1(b), empty, digit1(a)),
     snoc, b => tree(digit1(a), empty, digit1(b)),
     scan, scan(a),
-    foldl, (op,z) => foldl(op,z)(a)
+    foldl, (op,z) => foldl(op,z)(a),
+    size, size(a)
 ));    
 
 const empty = dispatch(list(
@@ -36,66 +34,73 @@ const empty = dispatch(list(
     cons, single,
     snoc, single,
     scan, null,
-    foldl, (op,z) => z
+    foldl, (op,z) => z,
+    size, 0
 ));
 
 const tree = (left, subtree, right) => dispatch(list(
   ".",  "tree(" + $(left) + "," + $(subtree) + "," + $(right) + ")",
-  cons, v => left(cons)(subtree, right)(v),
-  snoc, v => right(snoc)(left,subtree)(v),
+  cons, x => left(cons)(subtree, right)(x),
+  snoc, x => right(snoc)(left,subtree)(x),
   scan, flatmap(scan)(list(left, subtree, right)),
-  foldl, (op,z) => op(op(op(z,foldl(op,z)(left)),foldl(op,z)(subtree)),foldl(op,z)(right))
+  foldl, (op,z) => op(op(op(z,foldl(op,z)(left)),foldl(op,z)(subtree)),foldl(op,z)(right)),
+  size, size(left) + size(subtree) + size(right)
 ));
 
-const digit1 = v1 => dispatch(list(
-  ".", "[" + $(v1) + "]",
-  cons, (subtree, right) => v => tree(digit2(v,v1), subtree, right),
-  snoc, (left, subtree) => v => tree(left, subtree, digit2(v1,v)),
-  scan, flatmap(scan)(list(v1)),
-  foldl, (op,z) => foldl(op,z)(v1)
+const digit1 = a => dispatch(list(
+  ".", "[" + $(a) + "]",
+  cons, (subtree, right) => x => tree(digit2(x,a), subtree, right),
+  snoc, (left, subtree) => x => tree(left, subtree, digit2(a,x)),
+  scan, flatmap(scan)(list(a)),
+  foldl, (op,z) => foldl(op,z)(a),
+  size, size(a)
 ));
 
-const digit2 = (v1,v2) => dispatch(list(
-  ".", "[" + $(v1) + "," + $(v2) + "]",
-  cons, (subtree, right) => v => tree(digit3(v,v1,v2), subtree, right),
-  snoc, (left, subtree) => v => tree(left, subtree, digit3(v1,v2,v)),
-  scan, flatmap(scan)(list(v1,v2)),
-  foldl, (op,z) => op(foldl(op,z)(v1),foldl(op,z)(v2))
+const digit2 = (a,b) => dispatch(list(
+  ".", "[" + $(a) + "," + $(b) + "]",
+  cons, (subtree, right) => x => tree(digit3(x,a,b), subtree, right),
+  snoc, (left, subtree) => x => tree(left, subtree, digit3(a,b,x)),
+  scan, flatmap(scan)(list(a,b)),
+  foldl, (op,z) => op(foldl(op,z)(a),foldl(op,z)(b)),
+  size, size(a)+size(b)
 ));
 
-const digit3 = (v1,v2,v3) => dispatch(list(
-  ".", "[" + $(v1) + "," + $(v2) + "," + $(v3) + "]",
-  cons,(subtree, right) => v => tree(digit4(v,v1,v2,v3), subtree, right),
-  snoc,(left, subtree) => v => tree(left, subtree, digit4(v1,v2,v3,v)),
-  scan, flatmap(scan)(list(v1,v2,v3)),
-  foldl, (op,z) => op(op(foldl(op,z)(v1),foldl(op,z)(v2)),foldl(op,z)(v3))
+const digit3 = (a,b,c) => dispatch(list(
+  ".", "[" + $(a) + "," + $(b) + "," + $(c) + "]",
+  cons,(subtree, right) => x => tree(digit4(x,a,b,c), subtree, right),
+  snoc,(left, subtree) => x => tree(left, subtree, digit4(a,b,c,x)),
+  scan, flatmap(scan)(list(a,b,c)),
+  foldl, (op,z) => op(op(foldl(op,z)(a),foldl(op,z)(b)),foldl(op,z)(c)),
+  size, size(a)+size(b)+size(c)
 ));
 
-const digit4 = (v1,v2,v3,v4) => dispatch(list(
-  ".", "[" + $(v1) + "," + $(v2) + "," + $(v3) + "," + $(v4) + "]",
-  cons,(subtree, right) => v => tree(digit2(v,v1),subtree(cons)(node3(v2,v3,v4)),right),
-  snoc,(left, subtree) => v => tree(left,subtree(snoc)(node3(v1,v2,v3)),digit2(v4,v)),
-  scan, flatmap(scan)(list(v1,v2,v3,v4)),
-  foldl, (op,z) => op(op(op(foldl(op,z)(v1),foldl(op,z)(v2)),foldl(op,z)(v3)),foldl(op,z)(v4))
+const digit4 = (a,b,c,d) => dispatch(list(
+  ".", "[" + $(a) + "," + $(b) + "," + $(c) + "," + $(d) + "]",
+  cons,(subtree, right) => x => tree(digit2(x,a),subtree(cons)(node3(b,c,d)),right),
+  snoc,(left, subtree) => x => tree(left,subtree(snoc)(node3(a,b,c)),digit2(d,x)),
+  scan, flatmap(scan)(list(a,b,c,d)),
+  foldl, (op,z) => op(op(op(foldl(op,z)(a),foldl(op,z)(b)),foldl(op,z)(c)),foldl(op,z)(d)),
+  size, size(a)+size(b)+size(c)+size(d)
 ));
 
-const node2 = (v1,v2) => dispatch(list(
-  ".",  "(" + $(v1) + "," + $(v2) + ")",
-  scan, flatmap(scan)(list(v1,v2)),
-  foldl, (op,z) => op(op(z,foldl(op,z)(v1)),foldl(op,z)(v2))
+const node2 = (a,b) => dispatch(list(
+  ".",  "(" + $(a) + "," + $(b) + ")",
+  scan, flatmap(scan)(list(a,b)),
+  foldl, (op,z) => op(op(z,foldl(op,z)(a)),foldl(op,z)(b)),
+  size, size(a)+size(b)
 ));
 
-const node3 = (v1,v2,v3) => dispatch(list(
-  ".",  "(" + $(v1) + "," + $(v2) + "," + $(v3) + ")",
-  scan, flatmap(scan)(list(v1,v2,v3)),
-  foldl, (op,z) => op(op(op(z,foldl(op,z)(v1)),foldl(op,z)(v2)),foldl(op,z)(v3))
+const node3 = (a,b,c) => dispatch(list(
+  ".",  "(" + $(a) + "," + $(b) + "," + $(c) + ")",
+  scan, flatmap(scan)(list(a,b,c)),
+  foldl, (op,z) => op(op(op(z,foldl(op,z)(a)),foldl(op,z)(b)),foldl(op,z)(c)),
+  size, size(a)+size(b)+size(c)
 ));
 
 const build = n => n === 0 ? empty : build(n-1)(cons)(n);
 const t = build(30);
 display($(t));
-//display(scan(t));
-t(foldl)((x,y)=>x+y,1000000);
-//display(t(foldl)((x,y)=>x+y,1000000));
-//"ok";
+display(t(foldl)((x,y)=>x+y,0));
+display(size(t));
+"ok";
 
