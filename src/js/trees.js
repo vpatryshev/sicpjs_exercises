144f5aec81b886a740e130e27f7362e6590d360b
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2020-06-02 22:40:25 -0700

    Added null handling to tail as well using next abstraction

--- src/js/trees.js
+++ src/js/trees.js
@@ -1,10 +1,11 @@
-// Playground Session: https://api2.sourceacademy.nus.edu.sg/rees
+// Playground Session: https://api2.sourceacademy.nus.edu.sg/1m
 const tree = value => left => right => list(value, left, right);
 const not_defined = "N/A";
 const top = tree => is_null(tree) ? null : head(tree);
 const get_value = node => is_null(node) ? null : head(node);
-const left = tree => is_null(tree) ? not_defined : head(tail(tree));
-const right = tree => is_null(tree) ? not_defined : get_value(tail(tail(tree)));
+const next = node => is_null(node) ? null : tail(node);
+const left = tree => is_null(tree) ? not_defined : get_value(next(tree));
+const right = tree => is_null(tree) ? not_defined : get_value(next(next(tree)));
 
 // Tests
 const a = tree(4)(list(5, 7))(6);
