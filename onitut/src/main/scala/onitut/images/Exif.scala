b61d604ce39996e85e8c4f90a049fd3f4fbba67d
Vlad Patryshev <vpatryshev@gmail.com>
2021-01-22 12:58:26 -0800

    Exif documented

--- onitut/src/main/scala/onitut/images/Exif.scala
+++ onitut/src/main/scala/onitut/images/Exif.scala
@@ -31,6 +31,8 @@ case class Exif(path: Path) {
   val ourTimeZone: TimeZone = Calendar.getInstance.getTimeZone
 
   /**
+   * Extract image timestamp. Since there may be more than one, we take the earliest.
+   * 
    * @return image timestamp, as Date
    */
   def date: Option[Date] = {
@@ -45,6 +47,13 @@ case class Exif(path: Path) {
    */
   val timestamp: Option[Long] = date map (_.getTime) filter (0 < _)
 
+  /**
+   * Technical thing. Calculate a minimum of two optional values (or None, if none available)
+   * @param first first optional value
+   * @param second second optional value
+   * @tparam T data type
+   * @return minimum of two values, if present, or the one that is defined, or none
+   */
   def min[T <: Comparable[T]](first: Option[T], second: Option[T]): Option[T] = {
     (first, second) match {
       case (Some(a: T), Some(b: T)) => Some(if (a.compareTo(b) <= 0) a else b)
@@ -52,23 +61,31 @@ case class Exif(path: Path) {
     }
   }
 
-  def filterTimestamp(ts: Option[Date]): Option[Date] =
+  /**
+   * Making sure that the date we have is reasonable. Exif dates did not exist before year 2k.
+   * @param ts the optional timestamp we found in exif, may be wrong (e.g. year 0000)
+   * @return an optional timestamp; must be valid
+   */
+  private def filterTimestamp(ts: Option[Date]): Option[Date] =
     ts filter(_.getTime > Exif.TimeAfterWhichExifDateMakesSense)
-
-  def dt(date0: Option[Date], date1: Option[Date]): Long = (date0, date1) match {
-    case (Some(d0), Some(d1)) => Math.abs(d0.getTime - d1.getTime)
-    case _ => 0L
-  }
 }
 
 object Exif {
-  def timeOfYear(year: Int): Long = {
+  private def timeOfYear(year: Int): Long = {
     val c: Calendar = Calendar.getInstance()
     c.set(year, 0, 1)
     c.getTime.getTime
   }
-  
-  val MinPhotoTime: Long = timeOfYear(1820)
-  
-  val TimeAfterWhichExifDateMakesSense: Long = timeOfYear(2000)
+
+  /**
+   * Minimum time when the photo might have been taken.
+   * @see https://en.wikipedia.org/wiki/History_of_photography
+   */
+  val MinPhotoTime: Long = timeOfYear(1826)
+
+  /**
+   * Minimum time when exif started accompanying photos
+   * @see https://en.wikipedia.org/wiki/Exif#Background
+   */
+  val TimeAfterWhichExifDateMakesSense: Long = timeOfYear(1998)
 }
